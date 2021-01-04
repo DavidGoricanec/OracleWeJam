80282eaeea5a1d1e0aad5ba1c8f142aef7676097
David Goricanec <david.goricanec@edu.fh-joanneum.at>
2021-01-05 00:52:18 +0100

    login screen

---
+++ mobile/www/js/ajax.js
@@ -0,0 +1,38 @@
+var cloud_url = "https://vlthasl7otj6fl7-db202010251751.adb.eu-frankfurt-1.oraclecloudapps.com/ords/bandfinder/users";
+
+
+function set_session() {
+  var data;
+  var email = document.getElementById("inputEmail").value;
+  var pw = document.getElementById("inputPassword").value;
+
+  if (email == "" || email == null || pw == "" || pw == null)
+  {
+    alert("Please fill out both fields!");
+  }
+  else {
+    var request = new XMLHttpRequest();
+
+    request.open("POST",cloud_url+"/auth", false);
+    request.setRequestHeader("email",email);
+    request.setRequestHeader("password",pw);
+    request.addEventListener('load', function(event) {
+       if (request.status >= 200 && request.status < 300) {
+          data = JSON.parse(request.responseText);
+          if(data.status == 200)
+          {
+              //Login successfull
+              window.localStorage.setItem("session_key", data.session_key);
+              window.localStorage.setItem("session_expire_date", data.session_expire_date);
+              alert("Data: "+ window.localStorage.getItem("session_key"));
+          }
+          else {
+            alert(data.err_msg);
+          }
+       } else {
+          alert(request.responseText);
+       }
+    });
+    request.send();
+  }
+}
